8546a3992b333b72b74f1732c8c7ce48603663ba
Duc-Tin <tinldps40352@gmail.com>
2024-11-16 10:33:34 +0700

    Tin

--- app/src/main/java/com/viethcn/duanandroid/chitietsanpham.java
+++ app/src/main/java/com/viethcn/duanandroid/chitietsanpham.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.viethcn.duanandroid.Adapters.chitietsanphamAdapter;
-import com.viethcn.duanandroid.Models.Productchitiet;
+import com.viethcn.duanandroid.Adapters.ChiTietSanPhamAdapter;
+import com.viethcn.duanandroid.Models.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +16,8 @@ import java.util.List;
 public class chitietsanpham extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private chitietsanphamAdapter productAdapter;
-    private List<Productchitiet> productList;
+    private ChiTietSanPhamAdapter productAdapter;
+    private List<Product> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +25,25 @@ public class chitietsanpham extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chitietsanpham); // Ensure this layout file exists
 
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.recyclerViewSanPhamKhac);
 
         // Cài đặt GridLayoutManager với 2 cột
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        LinearLayoutManager gridLayoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
         recyclerView.setLayoutManager(gridLayoutManager);
 
         // Tạo danh sách sản phẩm mẫu
         productList = new ArrayList<>();
-        productList.add(new Productchitiet("Bánh bao đặc biệt", "35.000 VND", R.drawable.banhbao));
-        productList.add(new Productchitiet("Chả lụa", "140.000 VND", R.drawable.chalua));
-        productList.add(new Productchitiet(" Pate gan", "90.000 VND", R.drawable.pate));
-        productList.add(new Productchitiet("Xá xíu heo quay", "30.000/lạng", R.drawable.thitheo));
-        productList.add(new Productchitiet("Cơm cháy", "50.000 VND", R.drawable.comchay));
-        productList.add(new Productchitiet("Giò thủ", "150.000 VND", R.drawable.comchay));
+        productList.add(new Product( R.drawable.banhbao, "Bánh bao đặc biệt",35000));
+        productList.add(new Product(R.drawable.chalua,"Chả lụa",140000));
+        productList.add(new Product(R.drawable.pate,"Pate gan",90000));
+        productList.add(new Product(R.drawable.thitheo,"Xá xíu heo quay",300000));
+        productList.add(new Product(R.drawable.comchay,"Cơm cháy",50000));
+        productList.add(new Product(R.drawable.giothu,"Giò thủ",150000));
 
         // Thêm các sản phẩm khác vào productList...
 
         // Thiết lập Adapter cho RecyclerView
-        productAdapter = new chitietsanphamAdapter(productList, this);
+        productAdapter = new ChiTietSanPhamAdapter(productList, this);
         recyclerView.setAdapter(productAdapter);
     }
 
