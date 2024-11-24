6acd40e5039472372e6ee24ec9dfc9b30efec8e5
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-11-24 15:52:12 +0700

    cart

--- app/src/main/java/com/viethcn/duanandroid/Adapters/MainAdapter.java
+++ app/src/main/java/com/viethcn/duanandroid/Adapters/MainAdapter.java
@@ -1,8 +1,10 @@
 package com.viethcn.duanandroid.Adapters;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.viethcn.duanandroid.CartActivity;
 import com.viethcn.duanandroid.Models.MainModel;
 import com.viethcn.duanandroid.R;
 
@@ -26,24 +29,46 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.
         myViewHolder.name.setText(mainModel.getName());
         myViewHolder.price.setText(mainModel.getPrice());
 
+
+
         Glide.with(myViewHolder.img.getContext())
                 .load(mainModel.getImg())
                 .placeholder(com.google.android.gms.auth.api.R.drawable.common_google_signin_btn_icon_dark)
                 .circleCrop()
                 .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
                 .into(myViewHolder.img);
+
+        myViewHolder.btnBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), CartActivity.class);
+                intent.putExtra("name", mainModel.getName());
+                intent.putExtra("price", mainModel.getPrice());
+                intent.putExtra("img", mainModel.getImg());
+
+                v.getContext().startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.thucdon, parent, false);
+
         return new myViewHolder(view);
     }
 
     class myViewHolder extends RecyclerView.ViewHolder{
         CircleImageView img;
         TextView name, price;
+
+        Button btnBuyNow;
 
 
         public myViewHolder(@NonNull View itemView) {
@@ -52,6 +77,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.
             img = itemView.findViewById(R.id.menuItemImage);
             name = itemView.findViewById(R.id.menuItemName);
             price = itemView.findViewById(R.id.menuItemPrice);
+            btnBuyNow = itemView.findViewById(R.id.btnBuyNow);
         }
     }
 
